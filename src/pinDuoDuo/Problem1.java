75455871f2558ab6955e1061ba9f89477de75ff7
Mason <ms.catmissyou@gmail.com>
2018-12-15 16:49:08 +0800

    pinDuoDuo first question

---
+++ src/pinDuoDuo/Problem1.java
@@ -0,0 +1,13 @@
+package pinDuoDuo;
+/*
+     Author:Mason
+     2018/12/15 16:47
+     1、Alice、Bob、Cathy、Dave四个人排队喝可乐，喝完一个人变两个，接着继续到队尾排队，问第N个人喝可乐的人是谁
+
+如：N=8： ABCDAABB，第八个人是B，
+*/
+
+public class Problem1 {
+
+
+}
